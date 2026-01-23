b0fba8b2b4b20242b280924ce79941b96b4ddfd6
Oneiros96 <oneiros9692@gmail.com>
2026-01-23 12:32:43 +0100

    added doku for MainActivity.onCreate

--- app/src/main/java/com/oneiros/growapp/MainActivity.java
+++ app/src/main/java/com/oneiros/growapp/MainActivity.java
@@ -10,6 +10,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Overrides the parent onCreate Method to: <br>
+     *  - link the activity and its xml <br>
+     *  - enable Edge-to-Edge drawing of the app (introduced android 10, mandatory since android 15) <br>
+     *      ->bevor apps had a "safe box" between the sys bars <br>
+     *  - add a listener which adds padding corresponding to the sys bars <br>
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
