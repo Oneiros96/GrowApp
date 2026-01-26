516f72335f9cca6c58d6b916cbf426ff88aaeccc
Oneiros96 <oneiros9692@gmail.com>
2026-01-26 18:50:14 +0100

    plant/room dao

---
+++ app/src/main/java/com/oneiros/growapp/db/dao/PlantDao.java
@@ -0,0 +1,21 @@
+package com.oneiros.growapp.db.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.oneiros.growapp.db.entity.PlantEntity;
+
+import java.util.List;
+
+@Dao
+public interface PlantDao {
+    @Query("SELECT * FROM plants WHERE roomId = NULL")
+    LiveData<List<PlantEntity>> getPlantsWithoutRoom();
+    @Query("SELECT * FROM plants WHERE roomId = :roomId")
+    LiveData<List<PlantEntity>> getPlantsByRoom(int roomId);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(PlantEntity plant);
+}
---
+++ app/src/main/java/com/oneiros/growapp/db/dao/RoomDao.java
@@ -0,0 +1,19 @@
+package com.oneiros.growapp.db.dao;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import com.oneiros.growapp.db.entity.RoomEntity;
+
+import java.util.List;
+
+@Dao
+public interface RoomDao {
+    @Query("SELECT * FROM rooms ORDER BY name ASC")
+    LiveData<List<RoomEntity>> getAllRooms();
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(RoomEntity room);
+}
